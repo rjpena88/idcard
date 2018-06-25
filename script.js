f0e1a2333f743fb04bb1f6e9b812c4d7c7df1ed6
Remy Pena <rolandojpena88@gmail.com>
2018-06-25 12:03:05 -0500

    first commit to github

---
+++ script.js
@@ -0,0 +1,22 @@
+function idCard() {
+//First Name
+    let firstName =
+    document.getElementById("firstName").value;
+    document.getElementById("cardName").innerHTML = firstName;
+//Last Name
+    let lastName =
+    document.getElementById("lastName").value;
+    document.getElementById("cardLastName").innerHTML = lastName;
+//Age
+    let Age =
+    document.getElementById("Age").value;
+    document.getElementById("cardAge").innerHTML = Age;
+//Address
+    let Address =
+    document.getElementById("Address").value;
+    document.getElementById("cardAddress").innerHTML = Address;
+//Phone Number
+    let phoneNumber =
+    document.getElementById("phoneNumber").value;
+    document.getElementById("cardNumber").innerHTML = phoneNumber;
+}
